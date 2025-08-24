9afc961594f9600b66d1a5c73764f65e995f8901
lucas-jones-qa <lucas.jones@acct.global>
2025-08-24 18:17:22 -0300

    requisição do token para rodar os testes

--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,14 @@
-// In-memory user database
-const users = [];
+const bcrypt = require('bcryptjs');
+
+const users = [
+  { username: 'Lucas',
+    password: bcrypt.hashSync('123456', 8), 
+    favorecidos: ['test'],
+    saldo: 5000
+  }
+
+]
 
 module.exports = {
-  users,
+  users,  
 };
